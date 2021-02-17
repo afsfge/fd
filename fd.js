32e675f7cf517b3cfbb5a89d0a1b2127a9f54f5e
gechuanpeng <gechuanpeng@meituan.com>
2021-02-17 17:53:10 +0800

    fix

--- fd.js
+++ fd.js
@@ -3,10 +3,35 @@ let url=$request.url;
 
 console.log(url+":"+body);
 
+var freeAd={
+                "ADChapterEndCoinVideo": "1",
+                "ADChapterEndFreeVideo": "1",
+                "ADCodeBiKanChapterStartPop": "1",
+                "ADCodeBottomGuide": "1",
+                "ADCodeNewEndVideo": "1",
+                "ADCodeNewInsert": "1",
+                "ADCodeNewInsertVideo": "1",
+                "ADCodeNewUnlock": "1",
+                "ADCodeNewUnlockAuto": "1",
+                "ADCodeReadBottomFloat": "1",
+                "ADCodeReadEnd": "1",
+                "ADCodeReadEndIncentiveVideo": "1",
+                "ADCodeReadEndShadow": "1",
+                "ADCodeReadFullScreen": "1",
+                "ADCodeReadInsert": "1",
+                "ADCodeReadInsertFixed": "1",
+                "ADCodeReadInsertVideo": "1",
+                "ADCodeReadMiddlePop": "1",
+                "ADCodeReadStartIncentiveVideo": "1",
+                "ADCodeReadTopPop": "1",
+                "ADCodeReadTopVideoButton": "1",
+                "ADCodeSingleBookVideo": "1"
+            };
 if(url.endsWith("getInfo")){
 	var obj = JSON.parse(body);
 	obj.data.vip.is_vip="1";
 	obj.data.vip_end_time="4092579678000";
+	obj.data.vip.freeAd=freeAd;
 	body = JSON.stringify(obj); // 重新打包回json字符串
 }
 
