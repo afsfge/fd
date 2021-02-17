dc9e906c8d31c24d00c557acf5e9513696a1a794
gechuanpeng <gechuanpeng@meituan.com>
2021-02-17 17:16:07 +0800

    fix

--- fd.js
+++ fd.js
@@ -1,6 +1,6 @@
 var body = $response.body; // 声明一个变量body并以响应消息体赋
-var obj = JSON.parse(body); 
-obj.data.vip.is_vip="1";
-obj.data.vip.vip_end_time="4092579678000";
-body = JSON.stringify(obj); // 重新打包回json字符串
+
+body=body.replace(/is_vip\":\d+/g,'is_vip":1').replace(/vip_end_time\":\d/g,'vip_end_time":4092579678000');
+
+
 $done(body); // 结束修改
