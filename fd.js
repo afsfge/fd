5edd95be76dd2cc674a9f6fab64ac4237421f992
gechuanpeng <gechuanpeng@meituan.com>
2021-02-17 17:19:56 +0800

    fix

--- fd.js
+++ fd.js
@@ -1,6 +1,6 @@
 var body = $response.body; // 声明一个变量body并以响应消息体赋
 
 body=body.replace(/is_vip\":\d+/g,'is_vip":1').replace(/vip_end_time\":\d/g,'vip_end_time":4092579678000');
-console.log("飞读--->"body)
+console.log("飞读--->"body);
 
 $done(body); // 结束修改
