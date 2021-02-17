2ae693b85fe4988e59eeee5fc1889246194d9eaf
gechuanpeng <gechuanpeng@meituan.com>
2021-02-17 17:11:50 +0800

    fix

--- fd.js
+++ fd.js
@@ -1,3 +1,6 @@
 var body = $response.body; // 声明一个变量body并以响应消息体赋
-console.log("-----飞读-----")
-console.log("飞读:" + body)
+var obj = JSON.parse(body); 
+obj.data.vip.is_vip="1";
+obj.data.vip.vip_end_time="4092579678000";
+body = JSON.stringify(obj); // 重新打包回json字符串
+$done(body); // 结束修改
